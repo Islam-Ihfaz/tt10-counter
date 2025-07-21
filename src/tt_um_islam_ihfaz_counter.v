6f64c4d161c7d2dbb81f9e21618df3fccb985fe9
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-07-20 20:58:46 -0400

    Update tt_um_islam_ihfaz_counter.v

--- src/tt_um_islam_ihfaz_counter.v
+++ src/tt_um_islam_ihfaz_counter.v
@@ -53,6 +53,6 @@ module tt_um_islam_ihfaz_counter (
     assign uio_oe  = 0;
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, ui_in[7:1], uio_in 1'b0};
+    wire _unused = &{ena, ui_in[7:1], uio_in, 1'b0};
 
 endmodule
